16c189fdbb3d07105c1597f0aa222c7466263a42
fma <fma@30567597-2518-4260-b917-8f944d974ef6>
2013-05-16 14:19:53 +0000

    Fix too long path problem

--- epydoc.sh
+++ epydoc.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-epydoc -o ~/public_html/pKNyX -u http://www.pknyx.org -v -n pKNyX --no-imports --show-frames --graph all pknyx
+epydoc -o ~/public_html/pKNyX -u http://www.pknyx.org -v -n pKNyX --no-imports --show-frames --graph all --introspect-only pknyx
 

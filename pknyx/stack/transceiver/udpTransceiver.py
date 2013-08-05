569c26f7c4995d08174c0bc3569d6dd6060d786b
fma <fma@30567597-2518-4260-b917-8f944d974ef6>
2013-08-05 08:51:06 +0000

    Changed log

--- pknyx/stack/transceiver/udpTransceiver.py
+++ pknyx/stack/transceiver/udpTransceiver.py
@@ -203,7 +203,7 @@ class UDPTransceiver(Transceiver):
 
         self._transmitterSock.close()
 
-        Logger().trace("UDPTransceiver._transmitterLoop(): stopped")
+        Logger().trace("UDPTransceiver._transmitterLoop(): ended")
 
     def _receiverLoop(self):
         """
@@ -259,7 +259,7 @@ class UDPTransceiver(Transceiver):
 
         self._receiverSock.close()
 
-        Logger().trace("UDPTransceiver._receiverLoop(): stopped")
+        Logger().trace("UDPTransceiver._receiverLoop(): ended")
 
     def start(self):
         """
@@ -278,7 +278,7 @@ class UDPTransceiver(Transceiver):
         self._running = False
         self._receiver.join()
         self._transmitter.join()
-        Logger().trace("UDPTransceiver.stop(): stopped")
+        Logger().trace("UDPTransceiver.stop(): ended")
 
 
 if __name__ == '__main__':

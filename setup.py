deb8353d421ec7579aa270ba9ae12f111784108c
fma <fma@30567597-2518-4260-b917-8f944d974ef6>
2013-08-06 13:52:35 +0000

    First commit

---
+++ setup.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+
+""" Python KNX framework
+
+License
+=======
+
+ - B{pKNyX} (U{http://www.pknyx.org}) is Copyright:
+  - (C) 2013 Frédéric Mantegazza
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+or see:
+
+ - U{http://www.gnu.org/licenses/gpl.html}
+
+Module purpose
+==============
+
+Implements
+==========
+
+ - B{}
+
+Documentation
+=============
+
+Usage
+=====
+
+@author: Frédéric Mantegazza
+@copyright: (C) 2013 Frédéric Mantegazza
+@license: GPL
+"""
+
+__revision__ = "$Id: template.py 155 2013-07-10 12:34:24Z fma $"
+
+from distutils.core import setup
+
+from pknyx.common import config
+
+VERSION_PACKAGE = 1
+
+
+setup(name="pKNyX",
+      version="%s-%d" % (config.VERSION, VERSION_PACKAGE),
+      author="Frédéric Mantegazza",
+      author_email="fma@gbiloba.org",
+      url="http://www.pknyx.org",
+      description="Python KNX framework",
+      license="GPL",
+      long_description=open('README').read(),
+      #maintainer="Frederic Mantegazza",
+      #maintainer_email="fma@pknyx.org",
+      #download_url="http://www.pknyx.org/wiki/Download",
+      scripts=["multicast.sh"],
+      #package_dir={'pknyx': "pknyx"},
+      packages=["pknyx",
+                "pknyx.common",
+                "pknyx.core",
+                "pknyx.core.dptXlator",
+                "pknyx.examples",
+                "pknyx.examples.1_simple",
+                "pknyx.examples.2_simple_with_modules",
+                "pknyx.examples.3_timer",
+                "pknyx.examples.4_actuator",
+                "pknyx.plugins",
+                "pknyx.services",
+                "pknyx.stack",
+                "pknyx.stack.cemi",
+                "pknyx.stack.knxnetip",
+                "pknyx.stack.layer2",
+                "pknyx.stack.layer3",
+                "pknyx.stack.layer4",
+                "pknyx.stack.layer7",
+                "pknyx.stack.transceiver",
+                "pknyx.tools"],
+    #install_requires=[],
+)

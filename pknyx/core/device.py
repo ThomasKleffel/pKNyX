448ddddafd574b57180fcc5882525bf76127d3c8
Frédéric <fma@gbiloba.org>
2015-09-23 11:39:38 +0200

    Changed sleep delay

--- pknyx/core/device.py
+++ pknyx/core/device.py
@@ -81,7 +81,7 @@ class Device(object):
         Can be overriden to launch servers or so.
         """
         while True:
-            time.sleep(0.1)
+            time.sleep(0.001)
 
     def shutdown(self):
         """ Additionnal user shutdown
